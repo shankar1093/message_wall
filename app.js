39e348378fa2f222c6d4eaa7e6cc4136a2e73cf9
Shankar Rao <shankar1093@gmail.com>
2017-02-19 13:56:23 -0500

    moving the graph

---
+++ app.js
@@ -0,0 +1,15 @@
+var albumBucketName = 'BUCKET_NAME';
+var bucketRegion = 'REGION';
+var IdentityPoolId = 'IDENTITY_POOL_ID';
+
+AWS.config.update({
+  region: bucketRegion,
+  credentials: new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: IdentityPoolId
+  })
+});
+
+var s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {Bucket: albumBucketName}
+});
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var jsonObj = {
+      "hero": "CHO CHO",
+      "name": "Peter Benjamin Parker", 
+      "img":  "https://s3-us-west-2.amazonaws.com/ichodesign/gsjam/IMG_0274.JPG",
+      "size": 40000
+};
 
+function appendObj(obj){
+	var jsonFile = fs.readFileSync('marvel.json');
+	var _file = JSON.parse(jsonFile);
+	_file.children.push(obj);
+	var ichoJson = JSON.stringify(_file);
+	fs.writeFileSync('marvel.json', ichoJson);
+}
+
+
+appendObj(jsonObj);
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/'));
@@ -14,6 +31,13 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
+app.get('/upload', function(request, response) {
+  response.render('upload.html');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+
